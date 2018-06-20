a148a810177ece14ce6f3497142f72de27a826a9
omid reyhani <omid.reyhani@gmail.com>
2018-06-20 12:42:03 +0000

    ldsfh

--- resume-export.sh
+++ resume-export.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #resume export ./index.html --theme eloquent
 #resume export ./omid.pdf --theme eloquent --format pdf
-#resume export ./index.html --theme kendall 
+resume export ./index.html --theme kendall 
 #resume export ./omid.pdf --theme kendall --format pdf
-resume export ./index.html --theme eloquent 
-resume export ./omid.pdf --theme  papper --format pdf
+#resume export ./index.html --theme eloquent 
+resume export ./omid.pdf --theme  paper --format pdf
 
 
