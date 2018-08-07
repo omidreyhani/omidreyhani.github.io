4f44db6023bbefe3587749464bd49fe7b944652c
EC2 Default User <ec2-user@ip-172-31-15-246.us-west-2.compute.internal>
2018-08-07 08:57:34 +0000

    new date

--- resume-export.sh
+++ resume-export.sh
@@ -4,6 +4,6 @@
 resume export ./index.html --theme kendall 
 #resume export ./omid.pdf --theme kendall --format pdf
 #resume export ./index.html --theme eloquent 
-resume export ./omid.pdf --theme  paper --format pdf
+resume export ./omid-reyhani-resume.pdf --theme  paper --format pdf
 
 
