d93fa5ee9dfb86c92d93a18c57ce718e0ae79dbf
Omid Reyhani Shalmani <omid.reyhani@gmail.com>
2021-07-03 13:46:45 +0200

    repository

--- resume-export.sh
+++ resume-export.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+#npm install jsonresume-theme-paper
+
 #resume export ./index.html --theme eloquent
 #resume export ./omid.pdf --theme eloquent --format pdf
-resume export ./index.html --theme kendall 
+#resume export ./index.html --theme kendall 
 #resume export ./omid.pdf --theme kendall --format pdf
 #resume export ./index.html --theme eloquent 
 resume export ./omid-reyhani-resume.pdf --theme  paper --format pdf
+
+
+
+# resume export ./index1.html --theme caffeine
+# resume export ./index2.html --theme elegant
+resume export ./index.html --theme eloquent
+# resume export ./index4.html --theme even
+# resume export ./index5.html --theme flat
+# resume export ./index6.html --theme kendall
+# resume export ./index7.html --theme material
+# resume export ./index8.html --theme mocha-responsive
+# resume export ./index9.html --theme paper
+# resume export ./index10.html --theme slick
 
 
